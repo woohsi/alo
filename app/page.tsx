ef616419d255b3f3dc5095cbaac36dbc621ea9ba
Bo Hsiang <bottxiang@gmail.com>
2024-07-02 23:19:49 +0800

    update typo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="main">
       <blockquote className="m-5 mt-20">
         <p className="p-4 bg-gray-600 text-3xl rounded-md before:content-['\201C'] after:content-['\201D']">
-          aloalomac.com(alo alo mac) is comming soon...
+          aloalomac.com(alo alo mac) is coming soon...
         </p>
         <footer className="mt-2">—Aloha Mac, <cite><a href="https://www.facebook.com/bohsiang111" target="_blank">FB: <span className="underline">Aloha Ln</span></a></cite></footer>
       </blockquote>
